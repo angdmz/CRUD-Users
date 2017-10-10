5bdfb3a01a4334fea6f878e611f52527cf997b4d
Agustin Dorda <angdmz@gmail.com>
2017-10-09 21:50:32 -0300

    Agregados laravel-doctrine ORM y fluent

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace'=>"Api"], function () {
+    Route::get('/users','UsersController@index');
+    Route::post('/users','UsersController@store');
+    Route::patch('/users','UsersController@update');
+    Route::delete('/users','UsersController@delete');
 });
