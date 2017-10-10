1d443b4ef16096a5f1d21a818b401ac1819b172a
Agustin Dorda <angdmz@gmail.com>
2017-10-09 21:31:27 -0300

    Hecho controller usuarios, y routeos

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        return __METHOD__;
+    }
+
+    public function store(Request $request)
+    {
+        return __METHOD__;
+    }
+
+    public function update(Request $request)
+    {
+        return __METHOD__;
+    }
+
+    public function delete(Request $request)
+    {
+        return __METHOD__;
+    }
+}
